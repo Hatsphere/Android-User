981441177ec87457a089b3966796a4b169111391
jaya1001 <201551021@iiitvadodara.ac.in>
2017-10-18 15:12:23 +0530

    Added models module

---
+++ app/src/main/java/models/User.java
@@ -0,0 +1,53 @@
+package com.example.yashladha.android_user;
+
+public class User {
+
+    private long contact;
+    private String email;
+    private String name;
+    private String address;
+
+
+    public User(long contact, String email, String name, String address)
+    {
+        this.contact = contact;
+        this.email = email;
+        this.name = name;
+        this.address = address;
+
+    }
+
+    public long getContact()
+    {
+        return contact;
+    }
+
+    public void setContact(long contact)
+    {
+        this.contact = contact;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getAddress() { return address; }
+
+    public void setAddress(String address) { return address; }
+}
