cb75bc2ee82de5657b62ffb64a59f90bb36e40a2
yashLadha <201551061@iiitvadodara.ac.in>
2017-10-24 15:26:40 +0530

    Added cart adapter

---
+++ app/src/main/java/com/code/yashladha/android_user/Portal/Adapter/CartItemListAdapter.kt
@@ -0,0 +1,34 @@
+package com.code.yashladha.android_user.Portal.Adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.code.yashladha.android_user.Models.Item
+import com.code.yashladha.android_user.R
+
+/**
+ * Created by yashladha on 24/10/17.
+ * Adapter for inflating the cart list item
+ */
+
+class CartItemListAdapter(val items: ArrayList<Item>, val context: Context) : RecyclerView.Adapter<CartItemListAdapter.ViewHolder>() {
+
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int)  = holder!!.bind(items[position])
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.cart_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: Item) = with(itemView) {
+
+        }
+    }
+
+}
