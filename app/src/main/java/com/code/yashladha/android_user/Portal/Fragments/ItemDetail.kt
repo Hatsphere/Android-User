6eb67710c08fbc508afcccbbc74aba6903f2765f
yashLadha <201551061@iiitvadodara.ac.in>
2017-11-04 07:22:37 +0530

    updated item display
    Signed-off-by: yashLadha <201551061@iiitvadodara.ac.in>

---
+++ app/src/main/java/com/code/yashladha/android_user/Portal/Fragments/ItemDetail.kt
@@ -0,0 +1,13 @@
+package com.code.yashladha.android_user.Portal.Fragments
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.code.yashladha.android_user.R
+
+class ItemDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_detail)
+    }
+}
