da1ba36564cf6c040c50983c3334ab0f80393de7
yashLadha <201551061@iiitvadodara.ac.in>
2017-11-07 20:28:54 +0530

    chore: minor changes
    Signed-off-by: yashLadha <201551061@iiitvadodara.ac.in>

--- app/src/main/java/com/code/yashladha/android_user/Portal/Fragments/HomeFragment.kt
+++ app/src/main/java/com/code/yashladha/android_user/Portal/Fragments/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.code.yashladha.android_user.Portal.Fragments
 
 import android.app.Fragment
-import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.code.yashladha.android_user.Portal.Adapter.HomeIndexAdapter
 import com.code.yashladha.android_user.R
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
@@ -16,16 +19,22 @@ import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment: Fragment() {
 
+    private var horizontalLm: LinearLayoutManager? = null
+    private lateinit var interestedThingsRv : RecyclerView
+    private lateinit var interestedThingsAdap : HomeIndexAdapter
+
     companion object {
         val TAG = "HomeFragment"
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater!!.inflate(R.layout.fragment_home, container, false)
-        view.tvTemp.setOnClickListener {
-            val intent = Intent(view!!.context, ItemDetail::class.java)
-            startActivity(intent)
-        }
+        horizontalLm = LinearLayoutManager(view.context, LinearLayout.HORIZONTAL, false)
+        interestedThingsRv = view.home_rv_interested_things
+        interestedThingsRv.setHasFixedSize(true)
+
+        interestedThingsAdap = HomeIndexAdapter(ArrayList(), view.context)
+
         return view
     }
 
